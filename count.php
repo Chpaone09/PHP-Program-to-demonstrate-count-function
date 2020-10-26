3bc1a586cc92c48f1001f25cf59a3857eed1ec96
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-26 23:06:30 +0530

    Program to demonstrate count function in PHP

---
+++ count.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+***********************************************************
+
+Topic   : Program to demonstrate count function in PHP.
+Author  : Chpaone09®
+Date    : Oct 26, 2020
+
+************************************************************
+*/
+
+
+
+$a = array("name"=> "abc", "age"=>25);
+$b = array(5,3,4,1,2);
+
+$a1 = count($a);
+echo "<pre>";print_r($a1);
+echo "<br>";
+echo count($b);
+
+?>
